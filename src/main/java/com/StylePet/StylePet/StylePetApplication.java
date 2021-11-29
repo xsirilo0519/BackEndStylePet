52f82620084c5fcc3b73587adc6483a42bb6e8bb
sebas <sebaslpez0519@gmail.com>
2021-11-29 09:36:32 -0500

    Creacion del proyecto Spring boot

---
+++ src/main/java/com/StylePet/StylePet/StylePetApplication.java
@@ -0,0 +1,13 @@
+package com.StylePet.StylePet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylePetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylePetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/StylePet/StylePet/StylePetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StylePet.StylePet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StylePetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
